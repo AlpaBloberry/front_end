317362a2ac92578fb9951e0e07d9dd574e2d61ee
Alpa <alpaga.ytb@gmail.com>
2022-06-25 17:07:44 +0200

    Created the main page

---
+++ frontend/lib/widgets/leftColumn.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class leftColumn extends StatelessWidget {
+  const leftColumn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello");
+  }
+}
